699baf36dbdbdd82db5250af735e94704a536b46
Kacper Kłosowski <kacpermklosowski@gmail.com>
2024-11-26 17:35:56 +0100

    add interactivity with preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://kuos-astro-blog.netlify.app/"
+  site: "https://kuos-astro-blog.netlify.app/",
+  integrations: [preact()]
 });
